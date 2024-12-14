215fb2dc77c6ed3ce0f05043e8c2b8b6d7df088a
Edwxn <edwinpena0306@gmail.com>
2024-12-14 14:34:28 -0400

    arreglos

--- scripts/main.js
+++ scripts/main.js
@@ -8,21 +8,33 @@ function checkDNI() {
     const patron = /^\d{3}-\d{7}-\d$/;
 
     if(patron.test(inputValue) && inputValue != '') {
+        validDNI.classList.remove('blur-out');
         invalidDNI.classList.remove('active');
         validDNI.classList.add('active');
+        validDNI.classList.add('blur-in');
+
+        setTimeout(function() {
+            validDNI.classList.add('blur-out');
+        }, 2000);
 
         setTimeout(function() {
             validDNI.classList.remove('active');
-        }, 1400);
+        }, 2500);
     }
     
     else if(!patron.test(inputValue) || inputValue.length > patron.length) {
+        invalidDNI.classList.remove('blur-out');
         validDNI.classList.remove('active');
         invalidDNI.classList.add('active');
+        invalidDNI.classList.add('blur-in');
+
+        setTimeout(function() {
+            invalidDNI.classList.add('blur-out');
+        }, 2000);
 
         setTimeout(function() {
             invalidDNI.classList.remove('active');
-        }, 1400);
+        }, 2500);
     }
 }
 
